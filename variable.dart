a6d67b0c984e4792f7eeab0f8b46f4064fa30731
inixNsikak <53304138+inixNsikak@users.noreply.github.com>
2022-07-19 08:14:49 +0100

    Add files via upload

---
+++ variable.dart
@@ -0,0 +1,18 @@
+void main() {
+  //variable
+
+  int num1 = 23;
+  int num2 = 3;
+  int result = num1 + num2;
+  print(result);
+  //print(num2);
+  num result3;
+  result3 = 6;
+  print(result3);
+
+  //Code to print Date and time
+  final CurrentDay = DateTime.now().toUtc().toString();
+  print('Today is ' + CurrentDay);
+
+  //
+}
